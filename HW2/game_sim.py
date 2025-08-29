6a1a52f67bd8dec99a46a26a095e1044083647c9
johnkhall329 <johnk.hall329@gmail.com>
2025-08-29 17:01:19 -0400

    a* path planning and hero movement

--- HW2/game_sim.py
+++ HW2/game_sim.py
@@ -2,65 +2,121 @@ import pygame
 import sys
 import cv2
 import numpy as np
+import time
 
 from obstacle_field import FieldCreator
+from path_planner import get_path
 
 SIZE = 64
 WINDOW = 640
 
-def place_obj(obj,field, display:pygame.Surface, fail = False):
-    repeats = 0
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, role, map_loc):
+        super().__init__()
+        self.role = role
+        self.map_loc = map_loc
+        self.screen_loc = (self.map_loc[1]*(WINDOW/SIZE), self.map_loc[0]*(WINDOW/SIZE))
+        self.teleports = 0
+        self.image = pygame.Surface([WINDOW/SIZE,WINDOW/SIZE], pygame.SRCALPHA)
+        if role == "hero":
+            pygame.draw.circle(self.image,(0,0,255),(0.5*WINDOW/SIZE,0.5*WINDOW/SIZE), 4,0)
+        elif role == "enemy":
+            enemy_loc = np.array([((0.5*WINDOW/SIZE), 1), (1, WINDOW/SIZE-1),(WINDOW/SIZE-1, WINDOW/SIZE-1)], dtype=np.int16)
+            pygame.draw.polygon(self.image,(255,0,0),enemy_loc,0)
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft=self.screen_loc
+    
+    def update(self, enemies:pygame.sprite.Group):
+        if self.role == 'hero':
+            if self.progress < len(self.path):
+                next_node = self.path[self.progress]
+                self.progress += 1
+                self.map_loc = next_node
+            self.screen_loc = (self.map_loc[1]*(WINDOW/SIZE), self.map_loc[0]*(WINDOW/SIZE))
+            self.rect.topleft=self.screen_loc
+            surface = self.draw_path()
+            return surface
+    
+    def path_find(self, field, goal_loc):
+        try:
+            self.path = get_path(field, goal_loc, self.map_loc)
+            self.progress = 0
+            return self.draw_path()
+        except Exception as e:
+            print(e)
+    
+    def draw_path(self):
+        surface = pygame.Surface((WINDOW,WINDOW), pygame.SRCALPHA)
+        for i in range(self.progress, len(self.path)):
+            node = self.path[i]
+            prev_node = self.path[i-1]
+            pygame.draw.line(surface, (255,0,0), ((prev_node[1]+0.5)*WINDOW/SIZE, (prev_node[0]+0.5)*WINDOW/SIZE), ((node[1]+0.5)*WINDOW/SIZE, (node[0]+0.5)*WINDOW/SIZE))
+        return surface
+
+
+def place_obj(obj,field, display:pygame.Surface = None):
     new_field = np.copy(field)
     while True:
         loc = np.random.randint(64,size=2)
-        if field[loc[0],loc[1]]:
-            repeats +=1
-            if fail and repeats >= 5:
-                raise Exception("unable to safely place hero") 
-        else:
+        if not field[loc[0],loc[1]]:
             if obj.lower() == "goal":
                 new_field[loc[0],loc[1]] = 1
                 pygame.draw.rect(display, (0,255,0), pygame.Rect(loc[1]*(WINDOW/SIZE), loc[0]*(WINDOW/SIZE), (WINDOW/SIZE), (WINDOW/SIZE)))
+                sprite = tuple(loc)
             elif obj.lower() == "hero":
-                new_field[loc[0],loc[1]] = 1
-                pygame.draw.circle(display,(0,0,255),[((loc[1]+0.5)*(WINDOW/SIZE)), ((loc[0]+0.5)*(WINDOW/SIZE))], 4,0)
+                sprite = Sprite("hero", tuple(loc))
             else:
-                enemy_loc = np.array([[((loc[1]+0.5)*(WINDOW/SIZE)), (loc[0]*(WINDOW/SIZE))+1], [(loc[1]*(WINDOW/SIZE))+1, ((loc[0]+1)*(WINDOW/SIZE))-1],[((loc[1]+1)*(WINDOW/SIZE))-1, ((loc[0]+1)*(WINDOW/SIZE))-1]],dtype=np.int16)
-                pygame.draw.polygon(display,(255,0,0),enemy_loc,0)
-                loc -= [1,1] # shift to center of kernel
-                val = np.array([[  0,  50,  100,  50,   0],
-                                [ 50, 100, 150, 100,  58],
-                                [ 100, 150, 200, 150,  100],
-                                [ 50, 100, 150, 100,  50],
-                                [  0,  50,  100,  50,   0]], dtype=np.uint8)
+                sprite = Sprite("enemy", tuple(loc))
+                loc -= [2,2] # shift to center of kernel
+                val = np.array([[  0,  25,  50,  25,   0],
+                                [ 25, 50, 75, 50,  25],
+                                [ 100, 75, 100, 75,  100],
+                                [ 25, 50, 75, 50,  25],
+                                [  0,  25,  50,  25,   0]], dtype=np.uint8)
                 for idx, weight in np.ndenumerate(val):
                     if loc[0]+idx[0]<SIZE and loc[1]+idx[1]<SIZE:
                         if field[loc[0]+idx[0],loc[1]+idx[1]] not in [1,255]:
                             new_field[loc[0]+idx[0],loc[1]+idx[1]] += weight
-            return new_field
+            return new_field, sprite
     
 def main():
     pygame.init()
 
     display_surface = pygame.display.set_mode((WINDOW, WINDOW))
-    fc = FieldCreator(display_surface, False)
+    game_surface = pygame.Surface((640,640),pygame.SRCALPHA)
+    fc = FieldCreator(game_surface, False)
     field = fc.createField(0.2, 64)
-
     running = True
-    goal_field = place_obj('goal', field, display_surface, False)
-    hero_field = place_obj('hero', goal_field, display_surface,False)
-    cv2.namedWindow('field', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('field', 720,720)
-    enemy_field = place_obj('enemy', hero_field, display_surface,False)
-    for i in range(9):
-        enemy_field = place_obj('enemy', enemy_field, display_surface,False)
+    goal_field, goal_loc = place_obj('goal', field, game_surface)
+    hero_group = pygame.sprite.GroupSingle()
+    enemy_list = pygame.sprite.Group()
+    game_field, hero = place_obj('hero', goal_field)
+    hero_group.add(hero)
+    # cv2.namedWindow('field', cv2.WINDOW_NORMAL)
+    # cv2.resizeWindow('field', 720,720)
+    
+    for i in range(10):
+        game_field, enemy = place_obj('enemy', game_field)
+        enemy_list.add(enemy)
+
+    path_surface = hero.path_find(game_field, goal_loc)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        cv2.imshow('field', enemy_field)
-        cv2.waitKey(1)
+        # cv2.imshow('field', game_field)
+        # cv2.waitKey(1)
+        
+        path_surface = hero.update(enemy_list)
+        display_surface.blits([(game_surface,(0,0)),(path_surface,(0,0))])
+        enemy_list.update(enemy_list)
+        hero_group.draw(display_surface)
+        enemy_list.draw(display_surface)
+        
+        
         pygame.display.flip()
+        time.sleep(0.5)
 
     pygame.quit()
     sys.exit()
--- HW2/path_planner.py
+++ HW2/path_planner.py
@@ -1,2 +1,38 @@
-import numpy as np
 from queue import PriorityQueue
+
+def get_path(field, goal_loc, start_loc):
+    frontier = PriorityQueue()
+    frontier.put((0,start_loc))
+    came_from = {}
+    cost_so_far = {}
+    came_from[start_loc] = None
+    cost_so_far[start_loc] = 0
+    field = field.astype('int64')
+
+    while not frontier.empty():
+        item = frontier.get()
+        curr_node = item[1]
+        
+        if curr_node == goal_loc:
+            return format_path(came_from, curr_node)
+        
+        for next_node in [(curr_node[0]-1, curr_node[1]),(curr_node[0], curr_node[1]+1),(curr_node[0]+1, curr_node[1]),(curr_node[0], curr_node[1]-1)]:
+            if 0<=next_node[0]<field.shape[0] and 0<=next_node[1]<field.shape[1] and field[next_node] != 255:
+                new_cost = cost_so_far[curr_node] + field[next_node] + 1
+                prev_cost = cost_so_far.get(next_node)
+                if prev_cost is None or new_cost < prev_cost:
+                    cost_so_far[next_node] = new_cost
+                    heuristic = abs(goal_loc[0]-next_node[0]) + abs(goal_loc[1]-next_node[1])
+                    priority = int(new_cost + heuristic)
+                    frontier.put((priority,next_node))
+                    came_from[next_node] = curr_node
+    
+    raise Exception("No Path Found")
+
+def format_path(came_from, goal_loc):
+    path = []
+    node = goal_loc
+    while node is not None:
+        path.insert(0,node)
+        node = came_from[node]
+    return path
