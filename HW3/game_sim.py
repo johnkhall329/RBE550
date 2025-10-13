dd5a596bd01e1f590703d47b810bcc1b8f2e3b71
johnkhall329 <johnk.hall329@gmail.com>
2025-10-13 15:59:35 -0400

    Working kinematics

--- HW3/game_sim.py
+++ HW3/game_sim.py
@@ -1,14 +1,16 @@
-import pygame
 import sys
 import cv2
+import pygame
 import numpy as np
 import time
 
 from obstacle_field import FieldCreator
 from path_planner import get_path
+from vehicles import Vehicle
 
 MAP = 12
 SIZE = 60
+FPS = 30
 
 def put_text(win, display_surface):
     # places game winning or losing text on screen
@@ -27,65 +29,41 @@ def put_text(win, display_surface):
     
 def main():
     pygame.init()
-    display_surface = pygame.display.set_mode((MAP*SIZE, MAP*SIZE))
-    game_surface = pygame.Surface((MAP*SIZE,MAP*SIZE),pygame.SRCALPHA)
-    fc = FieldCreator(game_surface, False)
-    field = fc.createField(0.1, MAP) # creates a 12x12 grid with 10% fill
+    clock = pygame.time.Clock()
+    fc = FieldCreator()
+    bin_field, color_field = fc.createField(0.1, MAP, SIZE) # creates a 12x12 grid with 10% fill
     running = True
-    goal_field, goal_loc = place_obj('goal', field, game_surface) # place goal
-    hero_group = pygame.sprite.GroupSingle()
-    enemy_list = pygame.sprite.Group()
-    game_field, hero = place_obj('hero', goal_field) # place hero
-    hero.goal_loc = goal_loc
-    hero_group.add(hero)
     # cv2.namedWindow('field', cv2.WINDOW_NORMAL)
     # cv2.resizeWindow('field', 640,640)
-    
-    for i in range(10): # places 10 enemies
-        game_field, enemy = place_obj('enemy', game_field)
-        enemy_list.add(enemy)
-
-    hero.path_find(game_field)
-    hero_loc = hero.map_loc
-    path_surface = hero.draw_path(hero_loc) # once path is found, draw path
-    display_surface.blits([(game_surface,(0,0)),(path_surface,(0,0))])
-    hero_group.draw(display_surface)
-    enemy_list.draw(display_surface)
-    pygame.display.flip()
+    cv2.imshow('Parking Lot', color_field)
+    U = [0,0]
+    car = Vehicle('truck',720,(30,90))
     while running:
         try:
-            path_surface = hero.update(enemy_list,game_field) # move hero and update path
-            display_surface.blits([(game_surface,(0,0)),(path_surface,(0,0))])
-            for enemy in enemy_list:
-                game_field = enemy.update(hero_group,game_field) # move enemies and update game field in case of junk
-            collisions = pygame.sprite.groupcollide(enemy_list, enemy_list, False, False)
-            for sprite1, collided in collisions.items(): # iterate through enemies to see if any collide together
-                for sprite2 in collided:
-                    if sprite1 != sprite2: # avoid self collisions
-                        game_field=sprite1.teardown(game_field)
-                        game_field=sprite2.teardown(game_field)
-            hero_group.draw(display_surface)
-            enemy_list.draw(display_surface)
-            if pygame.sprite.groupcollide(hero_group, enemy_list, False, False): # check if enemy has killed hero
-                print("Game Over")
-                put_text(False,display_surface)
-                running=False
-            if hero.map_loc == goal_loc: # check if hero has reached goal
-                put_text(True,display_surface)
-                running=False
+            dt = clock.tick(FPS) / 1000.0  # seconds per frame
+            key = cv2.waitKey(1)
+            
+            if key==ord('w'):
+                U[1] += 2
+            if key==ord('s'):
+                U[1] -= 2
+            if key==ord('a'):
+                U[0] = max(U[0]-np.deg2rad(5),-np.pi/3)
+            if key==ord('d'):
+                U[0] = min(U[0]+np.deg2rad(5),np.pi/3)
+            
+            # print(U)
+            car.update(U,dt)
+            game_field, collision_field = car.draw(color_field.copy(),bin_field.copy(),False)
+            cv2.imshow('Parking Lot', game_field)
                 
         except ValueError as e:
             print(e)
-            put_text(False,display_surface)
             running=False
         
         # cv2.imshow('field', game_field)
         # cv2.waitKey(1)
         
-        pygame.display.flip()
-        time.sleep(0.25)
-
-    pygame.quit()
     sys.exit()
 
 if __name__ == "__main__":
--- HW3/vehicles.py
+++ HW3/vehicles.py
@@ -6,86 +6,82 @@ CAR_COLOR = (255,0,0)
 PATH_COLOR = (255,150,0)
 
 class Vehicle():
-    def __init__(self, type, size, scale, origin):
+    def __init__(self, type, img_size, origin):
         self.type = type
-        self.scale = scale
-        self.img_size = size
+        self.img_size = img_size
+        scale = self.img_size/12/3
         if self.type == 'delivery':
             self.width = 0.57*scale
             self.height = 0.7*scale
             self.x = origin[0]
             self.y = origin[1]
+            self.wheelbase = 0
         elif self.type == 'car':
             self.width = 1.8*scale
             self.height = 5.2*scale
-            self.wheelbase = 2.8
+            self.wheelbase = 2.8*scale
             self.x = origin[0]
             self.y = origin[1] - self.wheelbase/2
         elif self.type == 'truck':
             self.width = 2.0*scale
             self.height = 5.4*scale
-            self.wheelbase = 3.4
+            self.wheelbase = 3.4*scale
             self.trailer_width = 2.0*scale
             self.trailer_height = 4.5*scale
             self.trailer_dist = 5.0*scale
             self.x = origin[0]
-            self.y = origin - self.wheelbase/2 + self.trailer_height
+            self.y = origin[1] - self.wheelbase/2 + self.trailer_height/2 + self.trailer_dist
             self.trailer_heading = math.pi/2
-            self.trailer_center = (self.x-(self.trailer_dist*math.cos(self.trailer_heading)), self.y-(self.trailer_dist*math.sin(self.trailer_heading)))
-            self.trailer_points = []         
-        self.box_points = []
+            self.trailer_center = (self.x-(self.trailer_dist*math.cos(self.trailer_heading)), self.y-(self.trailer_dist*math.sin(self.trailer_heading)))     
         self.path_box_points = []
         self.heading = math.pi/2 # point along y axis
         self.speed = 0.0
-        for i in [-1,1]:
-            for j in [-1,1]:
-                self.box_points.append((int(origin[0] + i*self.width/2), int(origin[1] + j*self.height/2)))
-                if self.type == 'truck':
-                    self.trailer_points.append((int(self.trailer_center[0] + i*self.trailer_width/2), int(self.trailer_center[1] + j*self.trailer_height/2)))
-        self.box_points = np.array(self.box_points)
-        self.path_box_points.append(self.box_points)
+        self.box = cv2.RotatedRect((origin[0],origin[1]),((self.height),(self.width)),np.rad2deg(self.heading))
+        self.path_box_points.append(np.int32(self.box.points()))
         if self.type == 'truck': 
-            self.trailer_points = np.array(self.trailer_points)
-            self.path_box_points.append(self.trailer_points)
+            self.trailer_box = cv2.RotatedRect((self.trailer_center[0],self.trailer_center[1]),((self.trailer_height),(self.trailer_width)),np.rad2deg(self.trailer_heading))
+            self.path_box_points.append(np.int32(self.trailer_box.points()))
                     
     def update(self, U, dt):
-        theta, a = U
-        v_dot = a  # acceleration
-        self.speed += v_dot * dt
-        phi_dot = self.speed * math.tan(theta)/self.wheelbase*dt if self.type != 'delivery' else phi_dot = theta * dt # simple bicycle model
+        theta, v = U
+        self.speed = v
+        phi_dot = self.speed * math.tan(theta)/self.wheelbase*dt if self.type != 'delivery' else theta * dt # simple bicycle model
         self.heading += phi_dot
-        dx = self.speed * math.sin(self.heading) * dt
-        dy = self.speed * math.cos(self.heading) *dt
+        dx = self.speed * math.cos(self.heading) * dt
+        dy = self.speed * math.sin(self.heading) *dt
         self.x += dx
         self.y += dy
         
-        for i,point in enumerate(self.box_points):
-            point = (point[0]+dx, point[1]+dy)
-            new_x = int((point[0]-self.x)*math.cos(phi_dot) - (point[1]-self.y)*math.sin(phi_dot) + self.x)
-            new_y = int((point[1]-self.y)*math.cos(phi_dot) + (point[0]-self.x)*math.sin(phi_dot) + self.y)
-            self.box_points[i] = (new_x,new_y)
-        self.path_box_points.append(self.box_points)
-                
+       
+        # print(self.box_points)
+        center = (self.x+(math.cos(self.heading)*self.wheelbase/2),self.y+(math.sin(self.heading)*self.wheelbase/2))
+        self.box.center = center
+        self.box.angle = np.rad2deg(self.heading)
+        self.path_box_points.append(np.int32(self.box.points()))
+            
         if self.type == 'truck':
-            dt_trailer = self.speed/self.trailer_dist*math.sin(self.trailer_heading,self.heading)
-            self.trailer_heading += dt_trailer
-            new_trailer_center = (self.x-(self.trailer_dist*math.cos(self.trailer_heading)), self.y-(self.trailer_dist*math.sin(self.trailer_heading)))
-            dx_trailer = new_trailer_center[0] - self.trailer_center[0]
-            dy_trailer =  new_trailer_center[1] - self.trailer_center[1]
-            self.trailer_center = new_trailer_center
-            for i,point in enumerate(self.trailer_points):
-                point = (point[0]+dx_trailer, point[1]+dy_trailer)
-                new_x = int((point[0]-self.trailer_center[0])*math.cos(dt_trailer) - (point[1]-self.y)*math.sin(dt_trailer) + self.trailer_center[0])
-                new_y = int((point[1]-self.y)*math.cos(dt_trailer) + (point[0]-self.x)*math.sin(dt_trailer) + self.trailer_center[1])
-                self.trailer_points[i] = (new_x,new_y)
-            self.path_box_points.append(self.trailer_points)
+            self.trailer_heading -= self.speed/self.trailer_dist*math.sin(self.trailer_heading-self.heading)*dt
+            self.trailer_center = (self.x-(self.trailer_dist*math.cos(self.trailer_heading)), self.y-(self.trailer_dist*math.sin(self.trailer_heading)))
+            self.trailer_box.center = self.trailer_center
+            self.trailer_box.angle = np.rad2deg(self.trailer_heading)
+            self.path_box_points.append(np.int32(self.trailer_box.points()))
         
             
     def draw(self,color_field, bin_field, path=True):
-        cv2.fillConvexPoly(color_field,self.box_points,CAR_COLOR,-1)
-        cv2.fillConvexPoly(bin_field,self.box_points, (255,255,255), -1)
+        pts = self.box.points().astype(np.int32).reshape((-1, 1, 2))
+        cv2.fillConvexPoly(color_field,pts,CAR_COLOR)
+        cv2.fillConvexPoly(bin_field,pts,(255,255,255))
+        center = (round(self.x+(math.cos(self.heading)*10)),round(self.y+(math.sin(self.heading)*10)))
+        cv2.arrowedLine(color_field,(round(self.x),round(self.y)),center,(0,0,255),2)
+        if self.type == 'truck': 
+            pts = self.trailer_box.points().astype(np.int32).reshape((-1, 1, 2))
+            cv2.fillConvexPoly(color_field,pts,CAR_COLOR)
+            cv2.fillConvexPoly(bin_field,pts, (255,255,255))
+            cv2.line(color_field,(round(self.x),round(self.y)),(round(self.trailer_center[0]),round(self.trailer_center[1])),(0,0,0),2)
         
         if path:
             for box in self.path_box_points:
                 cv2.drawContours(color_field,[box],0,PATH_COLOR,2)
+        
+        return color_field, bin_field
     
